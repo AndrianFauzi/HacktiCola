49826826098fe1a7a0d16a9ef7d820cccaaa8fdc
AndrianFauzi <96704256+AndrianFauzi@users.noreply.github.com>
2022-05-27 11:38:14 +0700

    Update cart.js

--- cart.js
+++ cart.js
@@ -59,7 +59,8 @@ function checkOut() {
         instruksi.style.textAlign = 'center'
         instruksi.style.padding = '20px'
         containerStruk.appendChild(instruksi)
-    
+        let buttonCheckout = document.getElementById('buttoncheckout')
+        buttonCheckout.disabled = true
         container.appendChild(containerStruk)
     }else{
         alert("Lengkapi Form Terlebih Dahulu!");
